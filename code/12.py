a0d38728d6da05c981693859e882e9a1e8c7ec50
FleyFawkes <fley1@wp.pl>
2021-03-25 14:00:05 +0100

    day 12

--- code/12.py
+++ code/12.py
@@ -6,27 +6,40 @@ def data_file():
 
 def chart(array):
     course = {"N": 0, "S": 0, "E": 0, "W": 0, "F": "E"}
+    rose = ["E", "S", "W", "N"]
     test = ["L", "R"]
     for arr in array:
         dire = arr[0]
-        num = arr[1:]
+        num = int(arr[1:])
         if dire in course:
+            if dire == "F":
+                course[course["F"]] += num
+                continue
             course[dire] += num
         if dire in test:
-            course["F"] = angle(num, dire)
+            course["F"] = angleCalc(num, dire, rose)
     north, south, east, west = course.get("N"), course.get("S"), course.get("E"), course.get("W")
-    charted =
+    ns = [north, south]
+    ew = [east, west]
+    ns.sort(reverse=True)
+    ew.sort(reverse=True)
+    ns = ns[0] - ns[1]
+    ew = ew[0] - ew[1]
+    return ns, ew
 
 
-def angle(rotation, change):
-    angle = ["E", "S", "W", "N"]
+def angleCalc(rotation, change, angle):
     arit = rotation // 90
     if change == "R":
         for ind in range(0, arit):
             x = angle.pop(0)
-            angle.insert(-1, x)
+            angle.append(x)
     if change == "L":
         for ind in range(0, arit):
             x = angle.pop()
             angle.insert(0, x)
     return angle[0]
+
+
+routing = chart(data_file())
+print(routing[0] + routing[1])
